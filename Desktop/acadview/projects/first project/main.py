8b087d8432ead86b13513c721e2b3c793815e62f
manpreet024 <virkm066@gmail.com>
2017-03-24 11:33:16 +0530

    this is project 1

---
+++ Desktop/acadview/projects/first project/main.py
@@ -0,0 +1,7 @@
+a=raw_input("Enter 1 for students_to_teacher, 2 for battleship, 3 for exam_stats")
+if a=='1':
+    import students_to_teacher
+elif a=='2':
+    import battleship
+else:
+    import exam_stats
